3ab385df20b92d1cd0ca401a850ee214810b4fb1
Sith Saly <32166233+ThomasVSaly@users.noreply.github.com>
2020-04-27 10:05:36 +1000

    Copied Client from Marie, partially adapted to receive input from Arduino audio recording

---
+++ Audio Recording Interactions (Thomas)/Client/index.js
@@ -0,0 +1,42 @@
+// This has been adapted from team member Marie Thorsen code [1]
+
+const io = require('socket.io-client');
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+
+const portNum = process.argv.slice(2)[0];
+
+if (!portNum) {
+    console.log("Need USB port for arduino");
+    process.exitCode = 1;
+
+} else {
+    const port = new SerialPort(portNum, {
+        baudRate: 115200
+    });
+    const parser = new Readline();
+    port.pipe(parser);
+    
+
+
+    var socket = io.connect('http://supparoo.uqcloud.net');
+    socket.on('connect', function () {
+        console.log("Connected");
+        // Reading info from arduino, then sending that info to the server tagging as "message"
+        parser.on('data', line => {
+            switch(line){
+                case "record":
+                    break;
+                case "stopRecord":
+                    
+                    socket.emit('message', line);
+                    break;
+            }
+            
+        });
+        // For debuggin purposes
+        
+    });
+}
+
+// end of adapted code [1]
---
+++ Audio Recording Interactions (Thomas)/Client/references.md
@@ -0,0 +1,2 @@
+# references
+[1] Sending Interface & Back-end Infrastructure (Marie)/ Client
