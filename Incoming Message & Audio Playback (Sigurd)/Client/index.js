8d82a6d6818eb64b4feef751f5371bb16a66b84e
Sigurd Sæther Sørensen <32162632+Sigurdssorensen@users.noreply.github.com>
2020-04-27 11:04:36 +1000

    added audio play functionality in client

--- Incoming Message & Audio Playback (Sigurd)/Client/index.js
+++ Incoming Message & Audio Playback (Sigurd)/Client/index.js
@@ -1,3 +1,43 @@
+// The following code has been adapted from Bryan Jennings 
+// post on how to record and play audio in JavaScript [1].
+
+const recordAudio = () => {
+  return new Promise(resolve => {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(stream => {
+        const mediaRecorder = new MediaRecorder(stream);
+        const audioChunks = [];
+
+        mediaRecorder.addEventListener("dataavailable", event => {
+          audioChunks.push(event.data);
+        });
+
+        const start = () => {
+          mediaRecorder.start();
+        };
+
+        const stop = () => {
+          return new Promise(resolve => {
+            mediaRecorder.addEventListener("stop", () => {
+              const audioBlob = new Blob(audioChunks);
+              const audioUrl = URL.createObjectURL(audioBlob);
+              const audio = new Audio(audioUrl);
+              const play = () => {
+                audio.play();
+              };
+
+              resolve({ audioBlob, audioUrl, play });
+            });
+
+            mediaRecorder.stop();
+          });
+        };
+
+        resolve({ start, stop });
+      });
+  });
+};
+
 // This has been adapted from team member Marie Thorsen code [1]
 
 const io = require('socket.io-client');
@@ -28,11 +68,11 @@ if (!portNum) {
         parser.on('data', line => {
             switch(line){
                 case "play":
-                    // play audio
+                    audio.play();
                     break;
             }
         });
-``
+
         socket.on('audio', payload => {
             // send rgb values file to Arduino
             audio = payload.audio;
