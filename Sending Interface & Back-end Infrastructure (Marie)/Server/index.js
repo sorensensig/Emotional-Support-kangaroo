100cf4e5c88d1878d2d0471dd928a1a1723223c6
Marie <54302204+s4540545@users.noreply.github.com>
2020-04-28 10:31:36 +1000

    Changed port from 3000 to 80

--- Sending Interface & Back-end Infrastructure (Marie)/Server/index.js
+++ Sending Interface & Back-end Infrastructure (Marie)/Server/index.js
@@ -26,6 +26,6 @@ io.on('connection', function (socket) {
     });
 });
 
-http.listen(3000, function () {
-    console.log("Listening on *:3000")
+http.listen(80, function () {
+    console.log("Listening on *:80")
 });
