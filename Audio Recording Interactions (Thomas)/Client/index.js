73b7aafebbf991ef2eb05e8651b198fbada4eebd
Sith Saly <32166233+ThomasVSaly@users.noreply.github.com>
2020-04-30 12:39:01 +1000

    fixed tiny error incode, forgot to include equals and -1 in the color picker, woudl have crashed due to array out of bounds

--- Audio Recording Interactions (Thomas)/Client/index.js
+++ Audio Recording Interactions (Thomas)/Client/index.js
@@ -62,7 +62,7 @@ if (!portNum) {
     socket.on('connect', function () {
         console.log("Connected");
         let counter = 0;
-        const colours = ["255,0,0",, "0,255,0", "0,0,255", "255,0,255", "255,255,0", "0,255,255"];
+        const colours = ["255,0,0", "0,255,0", "0,0,255", "255,0,255", "255,255,0", "0,255,255"];
         // Reading info from arduino, then sending that info to the server tagging as "message"
         parser.on('data', line => {
           // let recorder;
@@ -85,7 +85,7 @@ if (!portNum) {
                         //audio = await recorder.stop();
                     //})();
                     counter++;
-                    if (counter > colour.length){
+                    if (counter >= colour.length-1){
                         counter = 0;
                     }
                     let colour = colours[counter];
