963330ed5965b4ce695326bf72d8215902877925
Sigurd Sæther Sørensen <32162632+Sigurdssorensen@users.noreply.github.com>
2020-06-02 11:20:12 +1000

    uncomment comment cleanup

--- E-mories Combined/Microcrontroller/Microcrontroller.ino
+++ E-mories Combined/Microcrontroller/Microcrontroller.ino
@@ -15,6 +15,10 @@ MPU6050 mpu6050(Wire);
 int hapticPin = 33; //Value for Tuva
 int bendPin = A1;
 
+// FILL IN YOUR NAME HERE
+string user = "Sigurd";
+// FILL IN YOUR NAME HERE
+
 bool vibrating = false;
 bool audioPlayed = false;
 bool lightsOn = false;
@@ -22,22 +26,10 @@ bool messageReceived = false;
 bool isRecording = false;
 
 int bendVal = 0;
+int bendThreshold;
 
-// Values for Thomas
-//int bendThreshold = 80; 
-
-// Values for Tuva
-int bendThreshold = 3000; 
-
-// Values for Sigurd
-//int bendThreshold = 420; 
-
-// Values for Marie
-//int bendThreshold = 340;
-
-//float throwThreshold = 2.70;
-float throwThreshold = 2.00; //Works for Tuva
 float dropThreshold = 0.30;
+float throwThreshold = 2.70;
 
 int savingCounter = 0;
 
@@ -49,9 +41,28 @@ int tempGreen = 0;
 int tempBlue = 0;
 int arr[] = {redValue, greenValue, blueValue};
 
+switch(user) {
+  case "Sigurd":
+    bendThreshold = 420;
+    break;
+  case "Tuva":
+    bendThreshold = 3000;
+    throwThreshold = 2.00;
+    break;
+  case "Marie":
+    bendThreshold = 340;
+    break;
+  case "Thomas":
+    bendThreshold = 80;
+    break;
+}
+
 void setup() {
-  //Serial.begin(115200);
-  Serial.begin(9600); //Value working for Tuva  
+  if(user == "Tuva") {
+    Serial.begin(9600); //Value working for Tuva 
+  } else {
+    Serial.begin(115200);
+  }
 
   //Vibration and flex sensor setup
   pinMode(hapticPin, OUTPUT);
@@ -71,25 +82,25 @@ void loop() {
 
   bendVal = analogRead(bendPin);
 
-  //---------------------------------------------------------------
-  //CHANGE to this if value decreases when squeezed
-  //  if(messageReceived && bendVal < bendThreshold) {
-  //    listenToMessage();
-  //  } else {
-  //    if(bendVal < bendThreshold){
-  //      isRecording = true;
-  //      Record();
-  //    }
-  //  }
-  //------------------------------------------------------------------
-  //Working for Tuva
-  if(messageReceived && bendVal > bendThreshold) {
-    listenToMessage();
-  } else {
-    if(bendVal > bendThreshold){
-      isRecording = true;
-      Record();
+  if(user == "Tuva") {
+    // Working for Tuva
+    if(messageReceived && bendVal > bendThreshold) {
+      listenToMessage();
+    } else {
+      if(bendVal > bendThreshold){
+        isRecording = true;
+        Record();
+      }
     }
+  } else {
+   if(messageReceived && bendVal < bendThreshold) {
+     listenToMessage();
+   } else {
+     if(bendVal < bendThreshold){
+       isRecording = true;
+       Record();
+     }
+   }
   }
   
   if(!(arr[0] == 0 && arr[1] == 0 && arr[2] == 0) && !audioPlayed) {
@@ -103,7 +114,7 @@ void Record(){
   int minimumRecordTime = 3;
   
   while(isRecording) {
-    bendVal = analogRead(bendPin); //Read and save analog value from potentiometer
+    bendVal = analogRead(bendPin); //Read and save analog value from bend sensor
     digitalWrite(hapticPin, HIGH);
     delay(60);
     digitalWrite(hapticPin, LOW);
@@ -116,16 +127,18 @@ void Record(){
 
     lightsOn = !lightsOn;   
     delay(500);
-    //---------------------------------------------------------------
-    //CHANGE to this if value decreases when squeezed
-    //    if(recordingCounter > minimumRecordTime && bendVal < bendThreshold){
-    //      isRecording = false;
-    //    }
-    //----------------------------------------------------------------------------
-    //Working for Tuva
-    if(recordingCounter > minimumRecordTime && bendVal > bendThreshold){
-      isRecording = false;
+
+    if(user == "Tuva") {
+      //Working for Tuva
+      if(recordingCounter > minimumRecordTime && bendVal > bendThreshold){
+        isRecording = false;
+      }
+    } else {
+      if(recordingCounter > minimumRecordTime && bendVal < bendThreshold){
+        isRecording = false;
+      }
     }
+
     recordingCounter++;
   }
   lightUpAllLights(pixels.Color(0, 0, 0), 0);  
@@ -300,21 +313,19 @@ void selectColor() {
     
     lightUpAllLights(pixels.Color(R, G, B), 0);
     bendVal = analogRead(bendPin);
-    //-----------------------------------------------------
-    //Working for Tuva
-    if(bendVal > bendThreshold){ 
-      colorSelected = true;
-    }
 
-    //-----------------------------------------------------
-    //CHANGE to this if value decreases when squeezed
-    //if(bendVal < bendThreshold){ 
-    //  colorSelected = true;
-    //}
-    
+    if(user == "Tuva") {
+      // Working for Tuva
+      if(bendVal > bendThreshold){ 
+        colorSelected = true;
+      }
+    } else {
+      if(bendVal < bendThreshold){ 
+        colorSelected = true;
+      }
+    }
     delay(100);
 
-    
   }
   if (colorSelected){
     vibrate(); 
@@ -364,17 +375,17 @@ void adjustColor (int & R, int & G, int & B){
     delay(100);
 
     bendVal = analogRead(bendPin);
-    //-----------------------------------------------------
-    //Working for Tuva
-    if(bendVal > bendThreshold){ 
-      colorAdjusted = true;
-    }
 
-    //-----------------------------------------------------
-    //CHANGE to this if value decreases when squeezed
-    //if(bendVal < bendThreshold){ 
-    //  colorAdjusted = true;
-    //}
+    if(user == "Tuva") {
+      //Working for Tuva
+      if(bendVal > bendThreshold){ 
+        colorAdjusted = true;
+      }
+    } else {
+      if(bendVal < bendThreshold){ 
+        colorAdjusted = true;
+      }
+    }
   }
   if (colorAdjusted){
     vibrate(); 
